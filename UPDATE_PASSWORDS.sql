8215a74fd9072de253266bf595df25e527c77306
David2020525 <genspark_dev@genspark.ai>
2026-01-14 09:50:00 +0000

    ⚡ Add immediate fix instructions

---
+++ UPDATE_PASSWORDS.sql
@@ -0,0 +1,3 @@
+UPDATE users SET password_hash = '$2b$10$K8ZqY8Z8Z8Z8Z8Z8Z8Z8ZOqK5h7kqK5nHZ.rXqK9K7C4K5h7kqK5' WHERE email = 'david2020524@gmail.com';
+UPDATE users SET password_hash = '$2b$10$K8ZqY8Z8Z8Z8Z8Z8Z8Z8ZOqK5h7kqK5nHZ.rXqK9K7C4K5h7kqK5' WHERE email = 'producer@musichub.com';
+UPDATE users SET password_hash = '$2b$10$K8ZqY8Z8Z8Z8Z8Z8Z8Z8ZOqK5h7kqK5nHZ.rXqK9K7C4K5h7kqK5' WHERE email = 'user@musichub.com';
