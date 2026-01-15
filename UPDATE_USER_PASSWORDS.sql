da2d03f894732650bebe78248b231bcecce56781
David2020525 <genspark_dev@genspark.ai>
2026-01-15 11:39:51 +0000

    🧹 Remove all unnecessary .MD, .txt, .sql temp files per user request

--- UPDATE_USER_PASSWORDS.sql
+++
@@ -1,3 +0,0 @@
-UPDATE users SET password_hash = '$sha256$01c7a406b17509e31adb1209bc7d43b8$60927b3c9e39a1ca884e9137a4d6a82eef17ab1002dc635b56c8b3385bb29d5a' WHERE email = 'david2020524@gmail.com';
-UPDATE users SET password_hash = '$sha256$01c7a406b17509e31adb1209bc7d43b8$60927b3c9e39a1ca884e9137a4d6a82eef17ab1002dc635b56c8b3385bb29d5a' WHERE email = 'producer@musichub.com';
-UPDATE users SET password_hash = '$sha256$01c7a406b17509e31adb1209bc7d43b8$60927b3c9e39a1ca884e9137a4d6a82eef17ab1002dc635b56c8b3385bb29d5a' WHERE email = 'user@musichub.com';
--- generate_hash.cjs
+++
@@ -1,4 +0,0 @@
-const bcrypt = require('bcryptjs');
-const hash = bcrypt.hashSync('password123', 10);
-console.log('Bcrypt hash:');
-console.log(hash);
